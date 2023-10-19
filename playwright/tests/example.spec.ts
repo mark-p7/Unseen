6394c4ff2564b7af0d50ab99939d1c39507bfe5e
Ashkan Zahedanaraki <ashkanzahde@gmail.com>
2023-10-18 19:29:41 -0700

    fixes

--- playwright/tests/example.spec.ts
+++ playwright/tests/example.spec.ts
@@ -19,6 +19,9 @@
 
 import { test, expect } from '@playwright/test';
 
+
+
+
 test('test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await expect(page.getByRole('heading', { name: 'Home page' })).toBeVisible();
