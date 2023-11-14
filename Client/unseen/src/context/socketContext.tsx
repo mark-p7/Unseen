be959b0ec5883a476d77b80ce5228bde7a6481c3
Connor Young <connor-young@outlook.com>
2023-11-13 23:12:48 -0800

    - added socketContext - create interfaces for Message and SocketContext - Provider has socket object, 2D array of groupID keys and sockerID values, and array of messages

---
+++ Client/unseen/src/context/socketContext.tsx
@@ -0,0 +1,64 @@
+import * as socketIO from "socket.io-client";
+import { Context } from "@/context/userContext";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+interface IMessage {
+    text: string;
+    name: string;
+    id: string;
+    socketId: string;
+    groupId: string;
+    image?: string;
+}
+
+interface ISocketContext {
+    socket: socketIO.Socket | undefined;
+    groupUsers: any;
+    messages: { [key: string]: IMessage[] };
+}
+
+const initialData: ISocketContext = {
+    socket: undefined,
+    groupUsers: {},
+    messages: {},
+};
+
+const SocketContext = createContext<ISocketContext>(initialData);
+
+export function useSocket() {
+    return useContext(SocketContext);
+}
+
+export default function SocketProvider({ children,}: { children: React.ReactNode; }) {
+    const { userStatus, setUserStatus } = useContext(Context);
+    const [groupUsers, setGroupUsers] = useState({});
+    const [socket, setSocket] = useState<socketIO.Socket>();
+    const [messages, setMessages] = useState<{ [key: string]: IMessage[] }>({});
+
+    const { username } = userStatus.username;
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!username) {
+            router.replace("/");
+            return;
+        }
+        let socket = socketIO.connect("https://localhost:8080");
+        socket.on("receive-message", (data: IMessage) => {
+            setMessages((prev) => {
+                const newMessages = { ...prev };
+                newMessages[data.groupId] = [...(newMessages[data.groupId] ?? []), data];
+                return newMessages;
+            });
+        });
+        socket.on("users-response", (data) => setGroupUsers(data));
+        setSocket(socket);
+    }, []);
+
+    return (
+        <SocketContext.Provider value={{ socket, groupUsers, messages }}>
+            {children}
+        </SocketContext.Provider>
+    );
+}
